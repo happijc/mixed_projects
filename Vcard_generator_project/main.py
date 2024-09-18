637c986a50a8822d254aaf51287c515c07d759c0
happijc <132833717+happijc@users.noreply.github.com>
2024-09-18 19:53:07 +0200

    recovering

---
+++ Vcard_generator_project/main.py
@@ -0,0 +1,31 @@
+class builder:
+
+    def __init__(self, firstname, secondname, contact_info, other*):
+        
+        self.firstname = firstname
+        self.secondname = secondname
+        self.contactinfo = contact_info
+        self.other = other
+        pass
+
+#Here is he programm supposed 
+    def builder():
+
+        pass
+
+#Here i will put the programm which is supposed to draw the draft of the visitcard
+    def drawer(dataFrame):
+
+        pass 
+
+    def main():
+        print('Please follow the instruction printed on your screen. ')
+        # userinput = input('Please submit the infos you ne')
+        user_firstname = input('What is your firstname ?')
+        user_secondname = input('Do you have a secondname ? How does it looks like ? ')
+        user_contact_info = input('Please enter your contact infos')
+        user_other_infos = input('Do you have any other infos you need to submit ?')
+
+        pass
+
+    pass
